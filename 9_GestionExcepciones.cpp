38a1ac98bb96ef6eeab38acf97886b4690e1922b
mllangon <mllangon@myuax.com>
2023-11-09 13:14:21 +0100

    9 public

--- 9_GestionExcepciones.cpp
+++ 9_GestionExcepciones.cpp
@@ -4,6 +4,13 @@
 using namespace std;
 
 class MateriaExcepcion{
-    private:
-        string mensaje;
+private:
+    string mensaje;
+
+public:
+    MateriaExcepcion(string mensaje) : mensaje(mensaje) {}
+
+    string getMensaje() const {
+        return mensaje;
+    }
 };
