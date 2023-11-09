3bb183a5f17aa724a908807334ecb832e0f7ce9c
mllangon <mllangon@myuax.com>
2023-11-09 12:40:49 +0100

    5 public

--- 5_registroMaterias.cpp
+++ 5_registroMaterias.cpp
@@ -10,3 +10,14 @@ public:
     void registrarMateria(const string& materia) {
         materias.push_back(materia);
     }
+
+    void listarmaterias() {
+        cout << nombre << " registra las siguientes materias";
+        for (size_t i = 0; i < materias.size(); i++) {
+            cout << " " << materias[i];
+            if  (i < materias.size() - 1){
+                cout << ",";
+            }
+        }
+        cout<<endl;
+    }
