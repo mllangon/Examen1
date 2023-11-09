7935286b5649200769d0b11bfd3a1a7d88fdd4f7
mllangon <mllangon@myuax.com>
2023-11-09 11:50:01 +0100

    1 operaciones

--- 1_OperacionesBasicas.cpp
+++ 1_OperacionesBasicas.cpp
@@ -8,11 +8,28 @@ int main(){
     cout<<"Ingrese el primer numero: ";
     cin>> a;
 
-    cout<<"Ingrese operacion: "
+    cout<<"Ingrese operacion: ";
     cin>> operacion;
 
     cout<<"Ingrese el segundo numero: ";
     cin>> b;
+
+    if (operacion == '+') {
+        cout << "Suma: " << a + b << endl;
+    } else if (operacion == '-') {
+        cout << "Resta: " << a - b << endl;
+    } else if (operacion == '*') {
+        cout << "Multiplicacion: " << a * b << endl;
+    } else if (operacion == '/') {
+        // Comprobar si el divisor es cero
+        if (b != 0) {
+            cout << "Division: " << a / b << endl;
+        } else {
+            cout << "Error: Division por cero." << endl;
+        }
+    } else {
+        cout << "Operacion no valida." << endl;
+    }
 
 
 
