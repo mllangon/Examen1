02b1515a8bb74f2713f729fc8fa9567cae79750f
mllangon <mllangon@myuax.com>
2023-11-09 13:03:57 +0100

    8 public

--- 8_FiltradoGrado.cpp
+++ 8_FiltradoGrado.cpp
@@ -8,5 +8,12 @@ private:
     string nombre;
     int grado;
 
-
+public:
+    Estudiante(string nombre, int grado) : nombre(nombre), grado(grado) {}
+    string getNombre() const {
+        return nombre;
+    }
+    int getGrado() const {
+        return grado;
+    }
 };
