594bf27b676b5aa6a562cfc3eb1152cb0da67e94
mllangon <mllangon@myuax.com>
2023-11-09 11:49:31 +0100

    1 arreglos

--- 1_OperacionesBasicas.cpp
+++ 1_OperacionesBasicas.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 
 int main(){
-    int a, b, suma, resta, multiplicacion, division;
+    double a, b;
+    char operacion;
 
     cout<<"Ingrese el primer numero: ";
     cin>> a;
 
+    cout<<"Ingrese operacion: "
+    cin>> operacion;
+
     cout<<"Ingrese el segundo numero: ";
     cin>> b;
 
-    suma = a + b;
-    resta = a - b;
-    multiplicacion = a * b;
-    division = a / b;
+
 
 
 }
