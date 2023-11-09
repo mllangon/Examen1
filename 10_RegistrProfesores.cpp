63f174f774d1a1991cae34768642d609ca8df83c
mllangon <mllangon@myuax.com>
2023-11-09 13:32:22 +0100

    10 public

--- 10_RegistrProfesores.cpp
+++ 10_RegistrProfesores.cpp
@@ -9,5 +9,13 @@ private:
     string materia;
     int experiencia;
 
+public:
+    Profesor(string nombre, int edad, string materia, int experiencia) : nombre(nombre), edad(edad), materia(materia), experiencia(experiencia) {}
 
+    void info_profesor(){
+        cout << "Nombre: " << nombre << endl;
+        cout << "Edad: " << edad << " anos" << endl;
+        cout << "Materia: " << materia << endl;
+        cout << "Experiencia: " << experiencia << " anos" << endl;
+    }
 };
