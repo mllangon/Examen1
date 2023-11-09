9bdc037edeee39d23a0f7213257814b10536259d
mllangon <mllangon@myuax.com>
2023-11-09 11:44:33 +0100

    1 couts

---
+++ 1_OperacionesBasicas.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, suma, resta, multiplicacion, division, residuo;
+
+    cout<<"Ingrese el primer numero: ";
+    cin>> a;
+
+    cout<<"Ingrese el segundo numero: ";
+    cin>> b;
+
+
+
+
+}
