07e35e3aed39aebe9c04c2676dc551e676490eaf
mllangon <mllangon@myuax.com>
2023-11-09 12:58:47 +0100

    7 public

--- 7_ManejoAusencias.cpp
+++ 7_ManejoAusencias.cpp
@@ -7,5 +7,12 @@ private:
     string fecha;
     string estado;
 
-
+public:
+    RegistroAusencias(string fecha, string estado) : fecha(fecha), estado(estado) {}
+    void infoRegistro() {
+        cout<<"Fecha de la clase: "<<fecha<<endl;
+        cout<<"Estado de la ausencia: "<<estado<<endl;
+    }
 };
+
+
