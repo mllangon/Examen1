e32296a6f193d61861bf6a6935ce533e4d1af420
mllangon <mllangon@myuax.com>
2023-11-09 12:54:54 +0100

    6 errores arreglados

--- 6_PromedioEstudiante.cpp
+++ 6_PromedioEstudiante.cpp
@@ -6,17 +6,17 @@ int main(){
     vector<int> notas = { 5, 7, 9, 1, 10};
 
     cout<<"Mis notas son:";
-    fot (int notas : calificaciones) {
-        cout << " " << notas;
+    for (int nota : notas) {
+        cout << " " << nota;
     }
     cout << endl;
 
     int suma = 0;
-    for (int notas : calificaciones) {
-        suma += notas;
+    for (int nota : notas) {
+        suma += nota;
     }
 
-    double promedio = static_cast <double> (suma) / calificaciones.size();
+    double promedio = static_cast<double> (suma) / notas.size();
 
     cout << "El promedio de las calificaciones del alumno es: " << promedio << endl;
     return 0;
