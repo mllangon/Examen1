b8aadcd7ee72eb66159e27876776e352671b6c45
mllangon <mllangon@myuax.com>
2023-11-09 12:01:52 +0100

    2 couts

--- 2_GestionEstudiantes.cpp
+++ 2_GestionEstudiantes.cpp
@@ -6,17 +6,19 @@ class Estudiante {
 private:
     string nombre;
     int edad;
-    string grado,
+    string grado;
 
 public:
-    void infoEstudiante(){
-        cout<<"Registra el nombre del estudiante: ";
-        cin>> nombre;
-        cout<<"Registra la edad del estudiante: ";
-        cin>> edad;
-        cout<<"Registra el grado del estudiante: ";
-        cin>> grado;
-    };
+    void infoEstudiante() {
+        cout << "Registra el nombre del estudiante: ";
+        cin >> nombre;
+        cout << "Registra la edad del estudiante: ";
+        cin >> edad;
+        cout << "Registra el grado del estudiante: ";
+        cin >> grado;
 
-
+        cout <<"Nombre: " << nombre << endl;
+        cout <<"Edad: " << edad <<"anos"<< endl;
+        cout <<"Grado: " << grado << endl;
+    }
 };
