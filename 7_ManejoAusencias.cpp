8509e467ea559461798d9a0ba0dc57867df429c1
mllangon <mllangon@myuax.com>
2023-11-09 12:59:51 +0100

    7 registros

--- 7_ManejoAusencias.cpp
+++ 7_ManejoAusencias.cpp
@@ -15,4 +15,18 @@ public:
     }
 };
 
+int main(){
+    RegistroAusencias registro1("12/03/2021","Ausente");
+    RegistroAusencias registro2("13/03/2021","Tarde");
+    RegistroAusencias registro3("14/03/2021","Tarde");
+    RegistroAusencias registro4("15/03/2021","Presente");
+    RegistroAusencias registro5("16/03/2021","Ausente");
 
+    registro1.infoRegistro();
+    registro2.infoRegistro();
+    registro3.infoRegistro();
+    registro4.infoRegistro();
+    registro5.infoRegistro();
+
+    return 0;
+}
